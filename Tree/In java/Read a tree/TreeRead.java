0634ad5cdff6d444943b7b2c73c2f6e03173cc73
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-04-26 10:09:00 +0530

    In-order traversal is added in the tree

--- Tree/In java/Read a tree/TreeRead.java
+++ Tree/In java/Read a tree/TreeRead.java
@@ -31,6 +31,12 @@ public class TreeRead {
         preOrder(node.left);
         preOrder(node.right);
     }
-    
+      // In-order traversal
+      public void inOrder(TreeNode node) {
+        if (node == null) return;
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
   
 }
