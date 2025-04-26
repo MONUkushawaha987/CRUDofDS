d772aa6926c5d03a0197849c694438f2418b6551
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-04-26 10:08:30 +0530

    pre-order traversal is added in the tree

---
+++ Tree/In java/Read a tree/TreeRead.java
@@ -0,0 +1,36 @@
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class TreeRead {
+    TreeNode root;
+    
+    public TreeRead() {
+        // Create a sample tree
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+    }
+    
+    // Pre-order traversal
+    public void preOrder(TreeNode node) {
+        if (node == null) return;
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    
+  
+}
