f4ad952bbefe221c71fe22d4a41384a9abf1e7b3
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-04-27 10:39:15 +0530

    adding vertices in graph

---
+++ Graph/In java/Create a graph/GraphCreate.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class GraphCreate {
+    private Map<Integer, List<Integer>> adjacencyList;
+
+    public GraphCreate() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjacencyList.putIfAbsent(vertex, new ArrayList<>());
+        System.out.println("Added vertex: " + vertex);
+    }
+
+    public void addEdge(int source, int destination) {
+        if (!adjacencyList.containsKey(source)) {
+            addVertex(source);
+        }
+        if (!adjacencyList.containsKey(destination)) {
+            addVertex(destination);
+        }
+        
+        adjacencyList.get(source).add(destination);
+        System.out.println("Added edge from " + source + " to " + destination);
+    }
+
+    public void printGraph() {
+        System.out.println("\nGraph Structure:");
+        for (int vertex : adjacencyList.keySet()) {
+            System.out.print(vertex + " -> ");
+            for (int neighbor : adjacencyList.get(vertex)) {
+                System.out.print(neighbor + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphCreate graph = new GraphCreate();
+        
+        // Adding vertices
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        
+       
+    }
+}
