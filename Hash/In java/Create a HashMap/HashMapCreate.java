1cb3eb0aab249e3da739a1d4ffb9322e1af50135
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-04-28 10:53:50 +0530

    Creating a hashmap in java

---
+++ Hash/In java/Create a HashMap/HashMapCreate.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class HashMapCreate {
+    public static void main(String[] args) {
+        // Create a HashMap
+        HashMap<String, Integer> studentScores = new HashMap<>();
+        
+        
+    }
+}
