691df1b67c85bcbc1f16d2f1d6faf8a648c8a975
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-04-28 10:54:39 +0530

    adding elements in  hashmap in java

--- Hash/In java/Create a HashMap/HashMapCreate.java
+++ Hash/In java/Create a HashMap/HashMapCreate.java
@@ -5,6 +5,12 @@ public class HashMapCreate {
         // Create a HashMap
         HashMap<String, Integer> studentScores = new HashMap<>();
         
+        // Add elements to HashMap
+        studentScores.put("Alice", 95);
+        studentScores.put("Bob", 88);
+        studentScores.put("Charlie", 76);
+        studentScores.put("Diana", 92);
+        
         
     }
 }
